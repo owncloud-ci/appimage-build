404a719e509aed79a83bad93fd0d168ce8f37414
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-09-15 12:27:07 +0200

    Install python 3.9.14 using pyenv

--- centos7/prepare_env.sh
+++ centos7/prepare_env.sh
@@ -2,4 +2,10 @@
 unset BASH_ENV PROMPT_COMMAND ENV
 # ensure we have a fully defined PKG_CONFIG_PATH before it is extended by devtoolset
 export PKG_CONFIG_PATH="$(pkg-config --variable pc_path pkg-config)"
+
+# setup pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 source scl_source enable devtoolset-9
