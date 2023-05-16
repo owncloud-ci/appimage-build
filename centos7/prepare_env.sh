ca2e68342e7a4481fc5a7227c0eab5a427de8f8d
Fabian Müller <fmueller@owncloud.com>
2023-05-16 16:19:25 +0200

    Fix sourcing devtoolset environment

--- centos7/prepare_env.sh
+++ centos7/prepare_env.sh
@@ -1,10 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 
 unset BASH_ENV PROMPT_COMMAND ENV
 # ensure we have a fully defined PKG_CONFIG_PATH before it is extended by devtoolset
-export PKG_CONFIG_PATH="$(pkg-config --variable pc_path pkg-config)"
+PKG_CONFIG_PATH="$(pkg-config --variable pc_path pkg-config)"
+export PKG_CONFIG_PATH
 
 if [[ "$PYENV_ROOT" != "" ]]; then
     export PATH="$PYENV_ROOT/bin:$PATH"
@@ -19,4 +20,4 @@ fi
 set +e
 
 # the devtoolscript is known fail, therefore we set +e before it
-source scl_source enable devtoolset-9
+source scl_source enable devtoolset-"${DEVTOOLSET_VERSION}"
